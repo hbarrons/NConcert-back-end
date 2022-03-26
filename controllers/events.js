0c1ca26134c526c453f03e72d007b78a30c60d68
sarahpicard <sarahannrey225@gmail.com>
2022-03-26 14:05:05 -0400

    add search functionality

--- controllers/events.js
+++ controllers/events.js
@@ -4,29 +4,22 @@ const baseUrl = "https://app.ticketmaster.com/discovery/v2"
 const apiKey = `${process.env.API_KEY}`
 
 function getEvents(req,res){
-  // if (req.params.page)
   axios.get(`${baseUrl}/events.json?classificationName=music&countryCode=US${apiKey}`)
   .then(response => {
     res.json(response.data._embedded.events)
   })
 }
 
-function getLinks(req,res){
-  axios.get(`${baseUrl}/events.json?classificationName=music&countryCode=US${apiKey}`)
-  .then(response => {
-    res.json(response.data)
+function getKeywordAndCity(req, res) {
+  axios.get(`${baseUrl}/events.json?classificationName=music&countryCode=US&keyword=${req.params.keyword}&city=${req.params.city}${apiKey}`)
+  .then(apiResponse => {
+    res.json(apiResponse.data)
   })
 }
-
-// function searchEvents (req,res){
-//   axios.get(`${baseUrl}/events.json?classificationName=music&countryCode=US${city}${keyword}${apiKey}`)
-//   .then(response => {
-//     res.json(response.data._embedded.events)
-//   })
-// }
 
 
 export {
   getEvents,
-  getLinks
+  // getLinks, 
+  getKeywordAndCity
 }
