e4116ccf9a5c2a3ecc2faa596a370092ab50d7f2
sarahpicard <sarahannrey225@gmail.com>
2022-03-28 12:19:17 -0400

    add show functionality for event detail page

--- controllers/events.js
+++ controllers/events.js
@@ -6,12 +6,17 @@ const apiKey = `${process.env.API_KEY}`
 function getSearch(req, res) {
   axios.get(`${baseUrl}/events.json?classificationName=music&countryCode=US&keyword=${req.params.keyword}&city=${req.params.city}&page=${req.params.page}&size=20${apiKey}`)
   .then(apiResponse => {
-    console.log("apiResponse: ", apiResponse)
+    // console.log("apiResponse: ", apiResponse)
     res.json(apiResponse.data)
   })
+}
+
+function show(req, res) {
+  console.log('sanity check')
 }
 
 
 export { 
   getSearch,
+  show,
 }
