63bfff82c21155581cc6b39deec91e2fde1453c6
hbarrons <hbarrons@gmail.com>
2022-03-30 14:52:58 -0400

    stub up delete event route/controller function

--- controllers/events.js
+++ controllers/events.js
@@ -4,10 +4,22 @@ const baseUrl = "https://app.ticketmaster.com/discovery/v2"
 const apiKey = `${process.env.API_KEY}`
 
 function getSearch(req, res) {
-  axios.get(`${baseUrl}/events.json?classificationName=music&countryCode=US&keyword=${req.params.keyword}&city=${req.params.city}&page=${req.params.page}&size=20${apiKey}`)
-  .then(apiResponse => {
-    res.json(apiResponse.data)
-  })
+  if (req.params.keyword === undefined) {
+    axios.get(`${baseUrl}/events.json?classificationName=music&countryCode=US&city=${req.params.city}&page=${req.params.page}&size=20${apiKey}`)
+    .then(apiResponse => {
+      res.json(apiResponse.data)
+    })
+  } else if (req.params.city === undefined) {
+    axios.get(`${baseUrl}/events.json?classificationName=music&countryCode=US&keyword=${req.params.keyword}&page=${req.params.page}&size=20${apiKey}`)
+    .then(apiResponse => {
+      res.json(apiResponse.data)
+    })
+  } else (
+    axios.get(`${baseUrl}/events.json?classificationName=music&countryCode=US&keyword=${req.params.keyword}&city=${req.params.city}&page=${req.params.page}&size=20${apiKey}`)
+    .then(apiResponse => {
+      res.json(apiResponse.data)
+    })
+  )
 }
 
 function show(req, res) {
